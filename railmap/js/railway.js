1d37f985f8e5aea70078afa65c039babae2d65be
shogomakino <sho5.makino@gmail.com>
2017-04-11 23:13:25 +0900

    update json format and add files

--- railmap/js/railway.js
+++ railmap/js/railway.js
@@ -1,29 +1,36 @@
-$(function(){
-    util.getJson(
-        'json_small_manual',
-        {
-            railway: "いわて銀河鉄道線",
-            operation_company: "アイジーアールいわて銀河鉄道"
-        },
-        function (errCode, result) {
-            //success
-            console.log(errCode);
-            console.log(result);
-            if (errCode) {
-                return;
-            }
-            var curve = result.Curve;
-            var str = "";
-            for(i in curve){
-                str += curve[i].lat + ", " + curve[i].lng + "<br>";
-            }
-            $("#disp_space").html(str);
-        },
-        function() {
-            //sent
-        },
-        function() {
-            //finished
-        }
+$(function() {
+    var pt = new google.maps.LatLng(41.925926, 143.240137);
+    var mapOptions = {
+        center: pt,
+        zoom: 7
+    };
+    var map = new google.maps.Map(
+        document.getElementById('disp_space'),
+        mapOptions
     );
+
+
+
+    $("#disp_button").click(function(e){
+        var section_id = $("#id_txt").val();
+        util.getJson(
+            'section_small.json',
+            {
+                gml_id: section_id
+            },
+            function (errCode, result){
+                console.log(errCode);
+                console.log(result);
+                if(errCode){ return; }
+                alert(result[0].railway_line_name);
+
+            },
+            function(){
+                //sent
+            },
+            function(){
+                //finished
+            }
+        );
+    });
 });
