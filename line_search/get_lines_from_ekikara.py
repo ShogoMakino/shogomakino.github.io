01143532a521d4baab879d414b149c107e4d9ac6
makino <makino@jsk.imi.i.u-tokyo.ac.jp>
2017-11-22 10:40:40 +0900

    add ekikara misc scripts

---
+++ line_search/get_lines_from_ekikara.py
@@ -0,0 +1,53 @@
+# coding: shift-jis
+#! /usr/bin/env python
+
+import requests
+import lxml.html
+
+def get_lines_in_pref(index):
+    url = 'http://ekikara.jp/newdata/state/line/%02d.htm' % index
+    response = requests.get(url)
+    response.encoding = "SHIFT_JIS"
+    root = lxml.html.fromstring(response.text)
+
+    elems = root.cssselect('.rsltSet01')
+    line_dict = {}
+    for elem in elems:
+        line_url_index = elem.cssselect('a')[0].attrib['href'].split('/')[-1].split('.')[0]
+        line_name = elem.cssselect('a')[0].text_content()
+        line_reading = elem.cssselect('span.m')[0].text_content()
+        line_dict[line_url_index] = [line_name, line_reading]
+
+    return line_dict
+
+def get_lines():
+    line_dict = {}
+    for i in range(47):
+        line_dict.update(get_lines_in_pref(i + 1))
+
+    return line_dict
+
+
+def write_js_file(line_dict):
+    f = open('line_data.js', 'w')
+    f.write('line_data = [')
+    first_flag = True
+    for elem in line_dict.items():
+        if not first_flag:
+            f.write(', ')
+        url_index=elem[0]
+        line_name=elem[1][0]
+        line_reading=elem[1][1]
+        line_str = "['%s', '%s', '%s']" % (url_index, line_name, line_reading)
+        f.write(line_str)
+        first_flag = False
+    f.write('];')
+    f.close()
+
+def main():
+    line_dict = get_lines()
+    write_js_file(line_dict)
+
+
+if __name__ == '__main__':
+    main()
---
+++ line_search/line_search.js
@@ -0,0 +1,16 @@
+function search(){
+    show_str = ""
+    text = document.f.line.value;
+    for(i = 0; i < line_data.length; i++){
+        if(line_data[i][1].indexOf(text) >= 0 || line_data[i][2].indexOf(text) >= 0){
+            show_str += button_str(line_data[i][0]) + line_data[i][1] + "<br>";
+        }
+    }
+    document.getElementById("result").innerHTML = show_str;
+}
+
+function button_str(url_index){
+    submit_button =  '<a href="javascript:ekikara_submit(' + url_index + ')">Submit</a>';
+    show_button = '<a href="http://ekikara.jp/newdata/line/' + url_index + '/down1_1.htm" target="blank">Show</a>';
+    return submit_button + show_button
+}
