273ce005729d0bf1edc0262664465f9bc191e8b1
makino <makino@jsk.imi.i.u-tokyo.ac.jp>
2018-02-21 10:53:16 +0900

    add search from url, add explanation

--- line_search/line_search.js
+++ line_search/line_search.js
@@ -1,9 +1,8 @@
-function search(){
+function search(search_text){
     show_str = ""
-    text = document.f.line.value;
     for(i = 0; i < line_data.length; i++){
-        if(line_data[i][1].indexOf(text) >= 0 || line_data[i][2].indexOf(text) >= 0){
-            show_str += button_str(line_data[i][0]) + line_data[i][1] + "<br>";
+        if(line_data[i][1].indexOf(search_text) >= 0 || line_data[i][2].indexOf(search_text) >= 0){
+            show_str += "<li>" + button_str(line_data[i][0]) + line_data[i][1] + "</li>";
         }
     }
     document.getElementById("result").innerHTML = show_str;
@@ -19,3 +18,18 @@ function ekikara_submit(url_index){
     document.submitform.lineNumber.value = url_index;
     document.submitform.submit();
 }
+
+function search_from_url(){
+    args = new Array();
+    location.search.substring(1).split('&').forEach(function(item){
+        var key_val_set = item.split('=');
+        args[key_val_set[0]] = decodeURI(key_val_set[1]);
+    });
+
+    if(args["line"]){
+        search(args["line"]);
+        document.f.line.value = args["line"];
+    }
+}
+
+window.onload=search_from_url;
